46e5be2148b7c16c00827722497ecc44562f3215
Trapper <matthias.linder@tptlive.ee>
2018-06-05 15:39:04 +0300

    Mouse Controls updated
    Mouse controls updated and the project was recreated and reorganized.

---
+++ Project---Kaiser-2.0/Assets/Scripts/Game Events/DestroyParentObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParentObject : MonoBehaviour {
+
+    void DestroytObject()
+    {
+        Destroy(this.gameObject.transform.parent.gameObject);
+    }
+}
