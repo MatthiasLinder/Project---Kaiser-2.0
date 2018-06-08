547f743c3edf135b8a674c75532fa8bb7fd4abe1
Trapper <matthias.linder@tptlive.ee>
2018-06-08 08:57:15 +0300

    Finishing touches
    Updated UI, created a Single Quest, Set an Objective trigger, added new terrain and added buildings.

---
+++ Kaiser - The RPG/Assets/Scripts/Quests/Quest001/Q001Objective1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Q001Objective1 : MonoBehaviour
+{
+    public GameObject TheObjective;
+    public int ClosedObjective;
+
+	void Update () {
+		if(ClosedObjective == 1)
+        {
+            if(TheObjective.transform.localScale.y <= 0.0f)
+            {
+                ClosedObjective = 0;
+                TheObjective.SetActive(false);
+            }
+            else
+            {
+                TheObjective.transform.localScale -= new Vector3 (0.0f, 0.01f, 0.0f);
+            }
+        }
+	}
+
+    void OnTriggerEnter( )
+    {
+        StartCoroutine(FinishObjective());
+    }
+
+    IEnumerator FinishObjective()
+    {
+        TheObjective.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        ClosedObjective = 1;
+    }
+}
+
+
---
+++ Kaiser - The RPG/Assets/Scripts/Quests/Quest001Take.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Quest001Take : MonoBehaviour {
+
+    public float TheDistance;
+    public GameObject ActionDisplay;
+    public GameObject ActionText;
+    public GameObject UIQuest;
+    public GameObject ThePlayer;
+    public GameObject NoticeCam;
+
+    void Update()
+    {
+        TheDistance = PlayerCasting.DistanceFromTarger;
+    }
+
+    void OnMouseOver()
+    {
+        if (TheDistance <= 3)
+        {
+            ActionDisplay.SetActive(true);
+            ActionText.SetActive(true);
+        }
+        if (Input.GetButtonDown("Action"))
+        {
+            if (TheDistance <= 3)
+            {
+                Screen.lockCursor = false;
+                Cursor.visible = true;
+                ActionDisplay.SetActive(false);
+                ActionText.SetActive(false);
+                UIQuest.SetActive(true);
+                NoticeCam.SetActive(true);
+                ThePlayer.SetActive(false);
+            }
+        }
+        ActionDisplay.SetActive(false);
+        ActionText.SetActive(false);
+    }
+
+    void OnMouseExit()
+    {
+        ActionDisplay.SetActive(false);
+        ActionText.SetActive(false);
+    }
+}
